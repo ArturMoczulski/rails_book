555149f118b0659d5b6ac1e530ce916252fb616e
Artur Moczulski <artur.moczulski@gmail.com>
2013-12-17 13:30:03 -0800

    initialized repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBook::Application.config.secret_key_base = '3089303469a148ccad5b9e816fbfe567fd2b1c0da8ba1ab9dbd8fedee8a6b5c4009e8214c75998c0a91a927605ee1b054ed2bcdb55398c2b8d047f15860af9d1'
